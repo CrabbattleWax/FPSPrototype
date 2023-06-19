6da6576e7cbf0e75cd54266c97fa990b1fc642c6
JR <jason.rieusset@compono.com>
2023-06-19 14:59:59 +1000

    This prototype was originally created as a way to recreate weapon recoil in game and some basic mechanics that were created relatively quickly. It includes some weapon types and a simple grenade mechanic.

---
+++ Assets/Scripts/GrenadeScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrenadeScript : MonoBehaviour {
+	
+
+	public float timeToDetonate = 2f;
+	public float explosionForce;
+	public float explostionRadius;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeToDetonate -= Time.deltaTime;
+		if(timeToDetonate < 0)
+        {
+			Explode();
+			Destroy(gameObject);
+        }
+	}
+	void Explode()
+    {
+		Vector3 explosionPos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere(explosionPos, explostionRadius);
+		foreach (Collider hit in colliders)
+		{
+			Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+			if (rb != null)
+				rb.AddExplosionForce(explosionForce, explosionPos, explostionRadius, 500.0F);
+		}
+	}
+}
---
+++ Assets/Scripts/TimeDestroyScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeDestroyScript : MonoBehaviour {
+	public float waitTime = 5f;
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds (waitTime);
+		Destroy (gameObject);
+	}
+
+}
